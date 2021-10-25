490bdc57eb034eaea8b52ab25478e8c4ab2a488e
Eng-Ahmad-Almohammad <almohammedahmed2@gmail.com>
2021-10-25 22:47:59 +0300

    .eslintrc.js has been added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,28 @@
-
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Providers from '../../context/GlobalProvider';
 import HomePage from "../HomePage/HomePage";
 
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 
+const App = () => {
 
-const App = ()=>{
-
-  return(
+  return (
     <Providers>
-    <Router>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-          
-        
-{/*        
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+
+
+          {/*        
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} /> */}
-       
 
-      </Switch>
-    </Router>
-   
-  </Providers>
+
+        </Switch>
+      </Router>
+
+    </Providers>
   )
 }
 
